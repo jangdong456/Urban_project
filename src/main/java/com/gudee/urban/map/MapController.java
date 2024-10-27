0b7c4107ca3937b26ea1c815418143c30dfec251
jangdongguk <jangdong456@gmail.com>
2024-10-27 20:57:18 +0900

    html test

--- src/main/java/com/gudee/urban/map/MapController.java
+++ src/main/java/com/gudee/urban/map/MapController.java
@@ -3,8 +3,10 @@ package com.gudee.urban.map;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
 
 //@RequiredArgsConstructor // 초기화 되지 않은 모든 final 필드 & @NonNull 마크가 있는 필드를 초기화하는 생성자 생성
@@ -18,10 +20,13 @@ public class MapController {
 	}
 	
 	@GetMapping("test")
-	public String getList(Model model) throws Exception {
+	public ModelAndView getList(Model model) throws Exception {
+		var modelAndView = new ModelAndView();
 		MapVO list = mapService.getList();
-		model.addAttribute("list", list);
 		
-		return "test";
+		
+		modelAndView.setViewName("test");
+		modelAndView.addObject(list);
+		return modelAndView;
 	}
 }
